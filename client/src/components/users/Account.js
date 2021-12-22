30a350f189e123ad0b4cb889a757b9282e3e8fd3
dani miletic <danimiletic@gmail.com>
2021-12-21 20:22:07 -0700

    added styles

--- client/src/components/users/Account.js
+++ client/src/components/users/Account.js
@@ -1,20 +1,35 @@
 import { UserConsumer } from '../../providers/UserProvider';
 import Moment from 'react-moment';
+import { Image, Card, Button } from 'react-bootstrap';
 
 const Account = ({ username, created_at, memberlvl, img }) => {
   return (
     <>
-      <h1>{username}</h1>
-      <img src={img} alt='profile image' />
-      <p>
-        Date Joined:
+      {/* <h1>{username}</h1> */}
+      <Image src={img} alt='profile image' thumbnail width="200px" />
+      {/* <p>
+        Date Joined: 
         <Moment format='MM/DD/YYYY'>
           {created_at}
         </Moment>
       </p>
       <p>
         Membership Level: {memberlvl}
-      </p>
+      </p> */}
+      <Card style={{ width: '18rem' }}>
+        <Card.Img variant="top" src={img} />
+        <Card.Body>
+          <Card.Title>{username}</Card.Title>
+          <Card.Text>
+            Date Joined: 
+            <Moment format='MM/DD/YYYY'>
+              {created_at}
+            </Moment>
+            <br />
+            Membership Level: {memberlvl}
+          </Card.Text>
+        </Card.Body>
+      </Card>
     </>
   )
 }
